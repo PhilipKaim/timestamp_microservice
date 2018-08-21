c5e792c48fa2af3e4254e2d90459ee90526a8797
Chillup <philipkaim@gmail.com>
2018-08-21 15:22:37 -0400

    GET /api/timestamp/:date

--- server/server.js
+++ server/server.js
@@ -4,6 +4,22 @@ const moment = require('moment');
 const app = express();
 const port = process.env.PORT || 3000;
 
+// 1451001600000
+
+app.get('/api/timestamp/:date', (req, res) => {
+    let timestamp = req.params.date;
+    let seconds = timestamp * 1000;
+    let date = new Date(seconds).toUTCString();
+
+    console.log(date);
+    
+
+    res.send({
+        unix: timestamp,
+        utc: date
+    });
+});
+
 app.get('/api/timestamp', (req, res) => {
     let date = new Date();
 
