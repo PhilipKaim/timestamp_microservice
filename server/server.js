a04aaaef12d0b2e3e40414c5a9077c73c29c6aea
Chillup <philipkaim@gmail.com>
2018-08-24 21:06:35 -0400

    final

--- server/server.js
+++ server/server.js
@@ -2,7 +2,7 @@ const path = require('path');
 const express = require('express');
 
 const app = express();
-const publicPath = path.join(__dirname, '..', 'views');
+const publicPath = path.join(__dirname, '..', 'public');
 const port = process.env.PORT || 2000;
 
 app.use(express.static(publicPath));
